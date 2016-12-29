5fe5cdc51d884cbbbb908bd687a47d081a39b403
Hormoz K <hkheradm@gmail.com>
2016-12-28 21:10:04 -0500

    okay

---
+++ button.py
@@ -0,0 +1,81 @@
+#!/bin/python
+
+import RPi.GPIO as GPIO
+import time
+import socket
+
+HOSTNAME = 'h8pc.local'
+PORT = 8098
+BUFF_SIZE = 1024
+I_LED = 20
+I_UP = 19
+I_DOWN = 5
+
+def refresh_socket(host, port):
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      s.connect((HOSTNAME, PORT))
+    except socket.error as e:
+      print("Could not connect to {}:{}. Retrying in 5 seconds".format(host, port))
+      time.sleep(5)
+      continue
+    print("Connected to {}:{}".format(host, port))
+    break
+  return s
+
+def idle():
+  GPIO.output(I_LED, True)
+
+def unidle():
+  GPIO.output(I_LED, False)
+  print "hk"
+
+def volume_up(socket):
+  socket.sendall("V-")
+
+def volume_up(socket):
+  socket.sendall("V+")
+
+def volume_down(socket):
+  socket.sendall("V-")
+
+
+def setup():
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setup(I_UP, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+  GPIO.setup(I_DOWN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+  GPIO.setup(I_LED, GPIO.OUT)
+
+def close():
+  GPIO.cleanup()
+
+def loop():
+  s = refresh_socket(HOSTNAME, PORT)
+  while True:
+    if GPIO.input(I_UP) == False:
+      unidle()
+      try:
+        volume_up(s)
+      except socket.error as e:
+        print("Disconnected due to: {}".format(e))
+        s.close()
+        s = refresh_socket(HOSTNAME, PORT)
+      time.sleep(0.1)
+    if GPIO.input(I_DOWN) == False:
+      unidle()
+      try:
+        volume_down(s)
+      except socket.error as e:
+        print("Disconnected due to: {}".format(e))
+        s.close()
+        s = refresh_socket(HOSTNAME, PORT)
+      time.sleep(0.1)
+    idle()
+
+if __name__ == "__main__":
+  try:
+    setup()
+    loop()
+  except KeyboardInterrupt:
+    close()
